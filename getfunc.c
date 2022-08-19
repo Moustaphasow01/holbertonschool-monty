2693341d960ffc95c5609f15cfc42865233b9095
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-19 14:09:43 +0200

    fixing task 0 bug

--- getfunc.c
+++ getfunc.c
@@ -32,6 +32,13 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
 	int num = 0, i;
+    if (data.words[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
+
 	
 		for (i = 0; data.words[1][i]; i++)
 	{
